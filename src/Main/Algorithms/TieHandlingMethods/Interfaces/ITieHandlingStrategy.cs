a82c8aafa74a9d5e9b0a8e86f0befb8dd5f4c19d
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-18 15:55:00 -0500

    Initial Commit

---
+++ src/Main/Algorithms/TieHandlingMethods/Interfaces/ITieHandlingStrategy.cs
@@ -0,0 +1,12 @@
+﻿using USC.GISResearchLab.Common.Core.Geocoders.ReferenceDatasets.Sources.Interfaces;
+using USC.GISResearchLab.Geocoding.Core.Algorithms.FeatureMatchingMethods;
+using USC.GISResearchLab.Geocoding.Core.Queries.Parameters;
+using USC.GISResearchLab.Geocoding.Core.ReferenceDatasets.ReferenceSourceQueries;
+
+namespace USC.GISResearchLab.Geocoding.Core.Algorithms.TieHandlingMethods.Interfaces
+{
+    public interface ITieHandlingStrategy
+    {
+        FeatureMatchingResult HandleTie(ParameterSet parameterSet, IFeatureSource source, ReferenceSourceQueryResultSet candidates);
+    }
+}
