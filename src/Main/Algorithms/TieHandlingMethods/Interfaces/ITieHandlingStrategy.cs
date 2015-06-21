91bb370c645c5faa6db8dbc4e0a927cbbf204d1d
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-20 20:56:18 -0500

    refactor of IFeatureSource and its fallout

--- src/Main/Algorithms/TieHandlingMethods/Interfaces/ITieHandlingStrategy.cs
+++ src/Main/Algorithms/TieHandlingMethods/Interfaces/ITieHandlingStrategy.cs
@@ -1,12 +1,13 @@
-﻿using USC.GISResearchLab.Common.Core.Geocoders.ReferenceDatasets.Sources.Interfaces;
+﻿
 using USC.GISResearchLab.Geocoding.Core.Algorithms.FeatureMatchingMethods;
 using USC.GISResearchLab.Geocoding.Core.Queries.Parameters;
 using USC.GISResearchLab.Geocoding.Core.ReferenceDatasets.ReferenceSourceQueries;
+using USC.GISResearchLab.Geocoding.Core.Metadata.FeatureMatchingResults;
 
 namespace USC.GISResearchLab.Geocoding.Core.Algorithms.TieHandlingMethods.Interfaces
 {
     public interface ITieHandlingStrategy
     {
-        FeatureMatchingResult HandleTie(ParameterSet parameterSet, IFeatureSource source, ReferenceSourceQueryResultSet candidates);
+        FeatureMatchingResult HandleTie(ParameterSet parameterSet, FeatureMatchingGeographyType featureMatchingGeographyType, ReferenceSourceQueryResultSet candidates);
     }
 }
