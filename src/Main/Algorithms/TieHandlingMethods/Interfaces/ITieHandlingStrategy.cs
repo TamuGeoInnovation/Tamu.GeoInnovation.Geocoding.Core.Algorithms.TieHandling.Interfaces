0039588194f87150b89b5d9cc9421039a3bfb85c
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2019-11-04 16:13:07 -0600

    updated json utils

--- src/Main/Algorithms/TieHandlingMethods/Interfaces/ITieHandlingStrategy.cs
+++ src/Main/Algorithms/TieHandlingMethods/Interfaces/ITieHandlingStrategy.cs
@@ -1,8 +1,8 @@
 ﻿
 using USC.GISResearchLab.Geocoding.Core.Algorithms.FeatureMatchingMethods;
+using USC.GISResearchLab.Geocoding.Core.Metadata.FeatureMatchingResults;
 using USC.GISResearchLab.Geocoding.Core.Queries.Parameters;
 using USC.GISResearchLab.Geocoding.Core.ReferenceDatasets.ReferenceSourceQueries;
-using USC.GISResearchLab.Geocoding.Core.Metadata.FeatureMatchingResults;
 
 namespace USC.GISResearchLab.Geocoding.Core.Algorithms.TieHandlingMethods.Interfaces
 {
